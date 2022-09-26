a82b89dda1a2cf4daf5a90ad3e1637fc15e13e90
Luis Eduardo Padilla <lalopamir@gmail.com>
2022-09-26 10:57:46 -0500

    ci: added sctpt file

---
+++ script.js
@@ -0,0 +1,6 @@
+        console.log("Hola!");
+        document.getElementById("generation").style.color = "red";
+        function colorTitulo() {
+            console.log("Pulsó boton Generation");
+            document.getElementById("generation").style.color = "blue";
+        }
